8087c2310c2cf29a4800596f1478350fc29643d7
Anna Fredriksson <leliloro@gmail.com>
2018-08-14 11:15:56 +0200

    move navbar to app

---
+++ src/app/components/md-books/md-books.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-md-books',
+  templateUrl: './md-books.component.html',
+  styleUrls: ['./md-books.component.scss']
+})
+export class MdBooksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
