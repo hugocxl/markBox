635954cc05eb62a602b2c2a40d97acc245da98b0
Anna Fredriksson <leliloro@gmail.com>
2018-08-15 19:26:37 +0200

    Add: New book with empty document to list

--- src/app/components/md-book-nav/md-book-nav.component.ts
+++ src/app/components/md-book-nav/md-book-nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MdBooksService } from '../../services/md-books.service';
+import { MdNotesService } from '../../services/md-notes.service';
+
 
 @Component({
   selector: 'app-md-book-nav',
@@ -9,22 +11,25 @@ import { MdBooksService } from '../../services/md-books.service';
 export class MdBookNavComponent implements OnInit {
 
   open = false;
-  mdBooks: Object;
+  mdBooks;
   mdNote : any;
   markdown: any;
+  newMdBook:any;
   id: number;
+
   newBook = {
     title : ''
   }
 
   constructor( 
-    private mdBooksService: MdBooksService    
+    private mdBooksService: MdBooksService,
+    private mdNotesService: MdNotesService  
   ) { }
 
   ngOnInit() {
     this.mdBooksService.getAll()
-      .then(mdBooks => {
-        this.mdBooks = mdBooks;
+      .then(mdBooksDB => {
+        this.mdBooks = mdBooksDB;
       })
       .catch(err => {
         console.error(err);
@@ -36,8 +41,19 @@ export class MdBookNavComponent implements OnInit {
   }
 
   addMdBook(form){
-    console.log(this.newBook);
-    //this.mdBooksService.new(this.newBook);
-  }
+    this.mdBooksService.new(this.newBook)
+    .then(book => {
+      this.newMdBook = book;
+      return this.mdNotesService.getOne(this.newMdBook.mdNotes[0])
+    })
+    .then(newNote =>{
+      this.newMdBook.mdNotes.push(newNote);
+      this.mdBooks.push(this.newMdBook);
+      this.newBook.title = '';
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  } 
 
 }
