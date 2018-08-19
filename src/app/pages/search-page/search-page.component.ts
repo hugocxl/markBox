d0e5a13264e9f0c0382a9d31203e827f48156654
Anna Fredriksson <leliloro@gmail.com>
2018-08-19 20:50:20 +0200

    Test home page pinned

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -4,7 +4,7 @@ import { MdNotesService } from '../../services/md-notes.service';
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
-  styleUrls: ['./search-page.component.scss']
+  styleUrls: ['./search-page.component.css']
 })
 export class SearchPageComponent implements OnInit {
 
