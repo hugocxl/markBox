6b74b5a62184a6d78653615d2bc8a69133cc22d8
Anna Fredriksson <leliloro@gmail.com>
2018-08-21 19:20:57 +0200

    Add background grid

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MdNotesService } from '../../services/md-notes.service';
+import { MdBooksService } from '../../services/md-books.service';
 
 @Component({
   selector: 'app-search-page',
@@ -9,15 +10,42 @@ import { MdNotesService } from '../../services/md-notes.service';
 export class SearchPageComponent implements OnInit {
 
   searchResult: any;
-
+  pinned
   constructor(
-    private mdNotesService: MdNotesService
+    private mdNotesService: MdNotesService,
+    private mdBooksService: MdBooksService
   ) { }
 
   ngOnInit() {
       this.mdNotesService.searchChange$.subscribe(updatedSearch => {
         this.searchResult = updatedSearch;
       })
+  }
+
+  delete(mdNote, mdBook){
+    console.log(mdNote);
+    console.log(mdBook);
+    this.mdNotesService.delete(mdNote._id)
+    .then(()=> {
+      this.mdBooksService.updateCurrentMdBook(mdBook._id);
+    })
+    .then(() =>{
+      this.mdBooksService.updateMdBooksList();
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  }
+
+  addToPinned(mdNote, mdBook){
+    mdNote.pinned = !mdNote.pinned;
+    this.mdNotesService.pin(mdNote._id, mdNote.pinned)
+    .then(() => {
+      this.mdBooksService.updateCurrentMdBook(mdBook._id);
+    })
+    .catch(error => {
+      console.error(error);
+    })
   }
 
 }
